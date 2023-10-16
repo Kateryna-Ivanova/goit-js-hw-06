a34952a4c94da0690039563cc1bf7d84eea63b59
Kateryna-Ivanova <katerinadialog@gmail.com>
2023-10-17 00:31:25 +0300

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,19 @@
 
+const form = document.querySelector(".login-form");
+const textAlert = 'Всі поля повинні бути заповнені'
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const Email = event.currentTarget.elements.email.value
+    // console.dir(Email);
+    const Password = event.currentTarget.elements.password.value
+    // console.dir(Password);
+    if (Password === "" || Email === "") {alert(textAlert) }
+    else {const dataForm = { Email,Password }
+        console.dir(dataForm)
+}
+    form.reset();
+}
